60763b073b0d6bba0286efe65389b04c3cc9423d
Fernando Lucas de Castro Cavalcante <117499108+namukuzedim@users.noreply.github.com>
2023-09-22 10:46:08 -0300

    att-
    p-aulaCK

--- 3 - React-Native-Expo/arquivos/src/configs/database.ts
+++
@@ -1,9 +0,0 @@
-const DATABASE_NAME = '@gameplay';
-
-const COLLECTION_USERS = `${DATABASE_NAME}:user`;
-const COLLECTION_APPOINTMENTS = `${DATABASE_NAME}:appointments`;
-
-export {
-  COLLECTION_USERS,
-  COLLECTION_APPOINTMENTS
-}
--- 3 - React-Native-Expo/arquivos/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://discord.com/api'
-});
-
-export { api }
