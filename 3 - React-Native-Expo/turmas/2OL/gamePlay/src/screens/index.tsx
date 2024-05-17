13d2fe0e3f092490fe70f842980faace0efc7b52
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2024-05-17 11:59:48 -0300

    beberes
    beberes

--- 3 - React-Native-Expo/turmas/2OL/gamePlay/src/screens/index.tsx
+++ 3 - React-Native-Expo/turmas/2OL/gamePlay/src/screens/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {View,Text,TextInput} from 'react-native'
 import {styles} from './styles'
 export  function SignIn() {
+
+  const [text,setText] = useState ("Você não digitou nada ainda");
+
   return (
      <View style={styles.container}>
        <Text> Ola mundo </Text>
-       <TextInput style={styles.input}/>
+
+       <TextInput style={styles.input} onChangeText={setText}/>
+
+       <Text> 
+        Você digitou: {text}
+       </Text>
      </View>
   );
 }
