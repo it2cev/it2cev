c650aebe7dbf7f22836067cc7e4c709a3063a0cf
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2025-08-27 09:25:18 -0300

    upgrade

--- 4 - React-Native-Expo/1-estilos-e-posicionamento/App.tsx
+++ 4 - React-Native-Expo/1-estilos-e-posicionamento/App.tsx
@@ -1,11 +1,21 @@
-import {View} from 'react-native';
+import {View,StyleSheet} from 'react-native';
 
 
 export default function App (){
 
     return(
-        <View style={{width:200,height:200,backgroundColor:'black',flex:1}}>
-            <View style={{width:200,height:200,backgroundColor:'pink'}}/>
+        <View style={styles.container}>
+            <View style={styles.box1}/>
+            <View style={styles.box2}/>
+            <View style={styles.box3}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{flex:1,width:200,height:200,backgroundColor:'black'},
+    box1:{width:200,height:200,backgroundColor:'white'},
+    box2:{width:200,height:200,backgroundColor:'red'},
+    box3:{width:200,height:200,backgroundColor:'yellow'},
+})
+
